eac3e6b2842318bf841596d93ac072f2984c33a6
godfreyJo <godfreyoyugi@gmail.com>
2019-02-11 18:55:31 +0300

    link review

--- app/src/main/java/com/gody/gody/busapp/MainActivity.java
+++ app/src/main/java/com/gody/gody/busapp/MainActivity.java
@@ -1,13 +1,15 @@
 package com.gody.gody.busapp;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,27 +29,28 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.home_user_login:
-                Intent myIntent = new Intent(this, user_login.class);
+                Intent myIntent = new Intent(this, com.gody.gody.busapp.user_login.class);
                 startActivityForResult(myIntent, 1);
                 break;
             case R.id.user_register:
-                Intent myIntent2 = new Intent(this, user_register.class);
+                Intent myIntent2 = new Intent(this, com.gody.gody.busapp.user_register.class);
                 startActivityForResult(myIntent2, 2);
                 break;
             case R.id.employee_login:
-                Intent myIntent3 = new Intent(this, employee_login.class);
+                Intent myIntent3 = new Intent(this, com.gody.gody.busapp.employee_login.class);
                 startActivityForResult(myIntent3, 3);
 
 
         }
     }
     @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        protected void onActivityResult(int requestCode, int resultCode, Intent data)
+        {
             if (requestCode == 1)
             {
                 if(requestCode == Activity.RESULT_OK)
                 {
-                    Intent home = new Intent(this, user_home.class);
+                    Intent home = new Intent(this, com.gody.gody.busapp.user_home.class);
                     String newText = data.getStringExtra("registrationNo");
                     home.putExtra("registrationNo", newText);
                     startActivity(home);
@@ -57,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
             {
                 if(requestCode == Activity.RESULT_OK)
                 {
-                    Intent myIntent = new Intent(this, user_login.class);
+                    Intent myIntent = new Intent(this, com.gody.gody.busapp.user_login.class);
                     startActivityForResult(myIntent, 1);
                 }
 
@@ -66,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             {
                 if(requestCode == Activity.RESULT_OK)
                 {
-                   Intent home = new Intent(this, employee_home.class);
+                   Intent home = new Intent(this, com.gody.gody.busapp.employee_home.class);
                    String newText = data.getStringExtra("employeeNo");
                    home.putExtra("employeeNo", newText);
                    startActivity(home);
@@ -74,16 +77,39 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             }
+
+
+
         }
 
     private void setClickListener()
     {
-       findViewById(R.id.home_user_login).setOnClickListener((View.OnClickListener) this);
-       findViewById(R.id.user_register).setOnClickListener((View.OnClickListener) this);
-       findViewById(R.id.home_user_login).setOnClickListener((View.OnClickListener)this);
+       findViewById(R.id.home_user_login).setOnClickListener(this);
+       findViewById(R.id.user_register).setOnClickListener(this);
+       findViewById(R.id.home_user_login).setOnClickListener(this);
 
     }
 
-    private void textStyles() {
+    private void textStyles()
+    {
+//        Get the elements
+        TextView welcome_to_textView = (TextView)findViewById(R.id.home_welcome_to);
+        TextView sib_textview          = (TextView)findViewById(R.id.home_sib);
+        Button user_login_button     = (Button)findViewById(R.id.home_user_login);
+        Button user_register_button  = (Button)findViewById(R.id.user_register);
+        Button employee_login_button = (Button)findViewById(R.id.employee_login);
+//        Find fonts
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/CaviarDreams.tff");
+//        Set fonts
+        welcome_to_textView.setTypeface(face);
+        sib_textview.setTypeface(face);
+        user_login_button.setTypeface(face);
+        user_register_button.setTypeface(face);
+        employee_login_button.setTypeface(face);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
