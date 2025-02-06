8111fac3fa2f22fad19ecf5ebf0dfe0cc069d9a0
G O <godfreyoyugi@gmail.com>
2025-02-06 09:46:17 +0300

    bookings app

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Seat, Booking
 
 # Register your models here.
+
+admin.site.register(Seat)
+admin.site.register(Booking)
+
+
--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
+from buses.models import Bus, Schedule
+
+
 
 # Create your models here.
+
+class Seat(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    seat_number = models.CharField(max_length=5)
+    is_booked = models.BooleanField(default=False)
+
+class Booking(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    scheduled = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add=True)
+    payment_status = models.BooleanField(default=False)
